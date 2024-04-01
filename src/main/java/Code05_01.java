b82370ee31687db5c8c1a24befa599e8f5d256bb
chloe-soyeon <soyeon96129@gmail.com>
2024-04-01 14:03:18 +0900

    0401수업

---
+++ src/main/java/Code05_01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Code05_01 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        if (num < 100) {
+            System.out.print("100보다 ");
+            System.out.print("작습니다.");
+        }
+   }
+}
+
