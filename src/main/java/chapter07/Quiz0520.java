64164504b98c6e461d7b446d783dd1baaaa86089
chloe-soyeon <soyeon96129@gmail.com>
2024-05-20 14:12:15 +0900

    0520코딩테스트

---
+++ src/main/java/chapter07/Quiz0520.java
@@ -0,0 +1,21 @@
+package chapter07;
+class Tree{
+    private String color;
+
+    public void setTree(String color){
+        this.color=color;
+    }
+}
+class AppleTree extends Tree{
+    public void setTree(String color) {
+        super.setTree(color);
+        System.out.printf("%s 색상으로 설정하였습니다.\n",color);
+    }
+}
+public class Quiz0520 {
+    public static void main(String[] args) {
+        AppleTree at = new AppleTree();
+        at.setTree("노란색");
+    }
+
+}
